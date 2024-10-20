c8ca6a84d03ff0a5d9d1198aa2ab9699dbcc8e99
RaminBgrn <ramin.bagheriian@gmail.com>
2024-10-20 18:40:28 +0330

    init splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chatify/home.dart';
+import 'package:chatify/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Home(),
+      home: SplashPage(
+        key: UniqueKey(),
+        onInitComplete: () {},
+      ),
     );
   }
 }
---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  final VoidCallback onInitComplete;
+
+  const SplashPage({super.key, required this.onInitComplete});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    _setup().then((_) => widget.onInitComplete);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.contain,
+              image: AssetImage('assets/images/logo_icon.png'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _setup() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    _registerServices();
+  }
+
+  void _registerServices() {}
+}
