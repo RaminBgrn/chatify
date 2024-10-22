4b9ce16555a0f3e1ad0d03d39dd1d4d656a8bc18
RaminBgrn <ramin.bagheriian@gmail.com>
2024-10-22 22:46:48 +0330

    login page form feild has been created

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:chatify/widgets/custom_input_fields.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,23 +12,34 @@ class _LoginPageState extends State<LoginPage> {
   late double _deviceHight;
   late double _deviceWidth;
 
+  final _loginFormKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     _deviceHight = MediaQuery.sizeOf(context).height;
     _deviceWidth = MediaQuery.sizeOf(context).width;
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: _deviceWidth * 0.03,
-          vertical: _deviceHight * 0.02,
-        ),
-        height: _deviceHight * 0.98,
-        width: _deviceWidth * 0.97,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [_pageTitle()],
+      body: GestureDetector(
+        onTap: () {
+          FocusManager.instance.primaryFocus?.unfocus();
+          // Focus.of(context).unfocus();
+        },
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: _deviceWidth * 0.03,
+            vertical: _deviceHight * 0.02,
+          ),
+          height: _deviceHight * 0.98,
+          width: _deviceWidth * 0.97,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _pageTitle(),
+              loginForm(),
+            ],
+          ),
         ),
       ),
     );
@@ -42,6 +54,32 @@ class _LoginPageState extends State<LoginPage> {
           color: Colors.white,
           fontSize: 40,
           fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+
+  Widget loginForm() {
+    return SizedBox(
+      height: _deviceHight * 0.18,
+      child: Form(
+        key: _loginFormKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            CustomInputFields(
+                onSaved: (str) {},
+                regEx: r"^[a-zA-Z0-9.a-z-A-Z0-9.!#$%&'*+-/=?^_`{|}]",
+                hintText: "Email",
+                obscure: false),
+            CustomInputFields(
+                onSaved: (str) {},
+                regEx: r".{8,}",
+                hintText: "Password",
+                obscure: true)
+          ],
         ),
       ),
     );
---
+++ lib/widgets/custom_input_fields.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class CustomInputFields extends StatelessWidget {
+  final Function(String value) onSaved;
+  final String regEx;
+  final String hintText;
+  final bool obscure;
+
+  const CustomInputFields(
+      {required this.onSaved,
+      required this.regEx,
+      required this.hintText,
+      required this.obscure,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onSaved: (saveValue) => onSaved(saveValue!),
+      cursorColor: Colors.white,
+      style: const TextStyle(color: Colors.white),
+      obscureText: obscure,
+      validator: (value) {
+        return RegExp(regEx).hasMatch(value!) ? null : "Enter valid value";
+      },
+      decoration: InputDecoration(
+        fillColor: const Color.fromRGBO(30, 29, 37, 1.0),
+        filled: true,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: BorderSide.none,
+        ),
+        label: Text(
+          hintText,
+          style: TextStyle(
+            fontSize: 14,
+            color: Colors.white54,
+          ),
+        ),
+      ),
+    );
+  }
+}
