bc36bb4a14644513856e9625028bb2f1b8f55ac5
RaminBgrn <ramin.bagheriian@gmail.com>
2024-11-04 20:44:24 +0330

    get message stream has been created and implimentat

--- lib/providers/chats_page_provider.dart
+++ lib/providers/chats_page_provider.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
+import 'dart:developer';
 import 'package:chatify/models/chat.dart';
+import 'package:chatify/models/chat_message.dart';
+import 'package:chatify/models/chat_user.dart';
 import 'package:chatify/providers/authentication_provider.dart';
 import 'package:chatify/services/database_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
@@ -16,7 +20,46 @@ class ChatsPageProvider extends ChangeNotifier {
     getChats();
   }
 
-  void getChats() async {}
+  void getChats() async {
+    try {
+      chatStream =
+          db.getChatsForUser(auth.userModel.uid).listen((snapshot) async {
+        chats = await Future.wait(snapshot.docs.map((doc) async {
+          Map<String, dynamic> chatData = doc.data() as Map<String, dynamic>;
+          // get users in chat
+          List<ChatUser> members = [];
+          for (var uid in chatData['members']) {
+            DocumentSnapshot userSnapShop = await db.getUser(uid);
+            Map<String, dynamic> userDataMap =
+                userSnapShop.data() as Map<String, dynamic>;
+            members.add(ChatUser.fromJSON(userDataMap));
+          }
+          // get last message for chats
+          List<ChatMessage> messages = [];
+          QuerySnapshot chatMessage = await db.getLastMessageForChat(doc.id);
+          if (chatMessage.docs.isNotEmpty) {
+            Map<String, dynamic> messageData =
+                chatMessage.docs.first.data()! as Map<String, dynamic>;
+            ChatMessage message = ChatMessage.fromJson(messageData);
+            messages.add(message);
+          }
+          // return chat instance
+          return Chat(
+            uid: doc.id,
+            currentUserUid: auth.userModel.uid,
+            members: members,
+            messages: messages,
+            activity: chatData['is_activity'],
+            group: chatData['is_group'],
+          );
+        }).toList());
+        notifyListeners();
+      });
+    } catch (e) {
+      log('Error getting chats');
+      log(e.toString());
+    }
+  }
 
   @override
   void dispose() {
